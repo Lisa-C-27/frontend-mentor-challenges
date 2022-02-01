7ff1b13f41f0d3996b0bc33b54dba08231cc2e77
Lisa-C-27 <sweet_kandi_27@hotmail.com>
2022-02-01 19:20:30 +1000

    Adding PWA

--- todo-app-vue/vue.config.js
+++ todo-app-vue/vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/frontend-mentor-challenges/todo-app-page/'
-    : '/'
+    : '/',
+    pwa: {
+      name: 'Todo App',
+      themeColor: 'purple',
+      msTileColor: '#000000',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+  
+      // configure the workbox plugin
+      workboxPluginMode: 'GenerateSW'
+    }
 }
