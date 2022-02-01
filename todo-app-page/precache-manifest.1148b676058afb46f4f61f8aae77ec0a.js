def24f20483326f30f86f7b8ecc9d68fdb3241ba
Lisa-C-27 <sweet_kandi_27@hotmail.com>
2022-02-01 19:21:11 +1000

    Adding PWA

---
+++ todo-app-page/precache-manifest.1148b676058afb46f4f61f8aae77ec0a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6b1896d5994ab944f4c",
+    "url": "/frontend-mentor-challenges/todo-app-page/css/app.4d201e6f.css"
+  },
+  {
+    "revision": "02105f02a8dba33f6c8bafac6a9b0979",
+    "url": "/frontend-mentor-challenges/todo-app-page/img/bg-desktop-dark.02105f02.jpg"
+  },
+  {
+    "revision": "988cb74c73a65433889e51b150c1a8f9",
+    "url": "/frontend-mentor-challenges/todo-app-page/img/bg-desktop-light.988cb74c.jpg"
+  },
+  {
+    "revision": "b0112ea5222ffd8c9d69349be56c620b",
+    "url": "/frontend-mentor-challenges/todo-app-page/img/bg-mobile-dark.b0112ea5.jpg"
+  },
+  {
+    "revision": "266f20c7fd7778c0b72ac804fd769fe5",
+    "url": "/frontend-mentor-challenges/todo-app-page/img/bg-mobile-light.266f20c7.jpg"
+  },
+  {
+    "revision": "5742160ca6ea943a508cc5763e8cd312",
+    "url": "/frontend-mentor-challenges/todo-app-page/img/icon-check.5742160c.svg"
+  },
+  {
+    "revision": "fe68d57f09f867dbbd1ff592c3b9a017",
+    "url": "/frontend-mentor-challenges/todo-app-page/img/icon-cross.fe68d57f.svg"
+  },
+  {
+    "revision": "e66b8c621b1f209e44a7c955c0e5e809",
+    "url": "/frontend-mentor-challenges/todo-app-page/img/icon-moon.e66b8c62.svg"
+  },
+  {
+    "revision": "b3182f3b0ccf1d65cae48b64861b83a9",
+    "url": "/frontend-mentor-challenges/todo-app-page/img/icon-sun.b3182f3b.svg"
+  },
+  {
+    "revision": "ac539cd0d68a8347b271bca2e95949ad",
+    "url": "/frontend-mentor-challenges/todo-app-page/index.html"
+  },
+  {
+    "revision": "d6b1896d5994ab944f4c",
+    "url": "/frontend-mentor-challenges/todo-app-page/js/app.ae4a6d6f.js"
+  },
+  {
+    "revision": "56a18450c69bacd0a5ae",
+    "url": "/frontend-mentor-challenges/todo-app-page/js/chunk-vendors.ac572a9b.js"
+  },
+  {
+    "revision": "e4fefd0d525617aa84c61f2d0affb75c",
+    "url": "/frontend-mentor-challenges/todo-app-page/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/frontend-mentor-challenges/todo-app-page/robots.txt"
+  }
+]);
